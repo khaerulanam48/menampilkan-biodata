25cdef459a5f40d5ef0692cc58229c5eac007ad3
khaerulanam48 <48309539+khaerulanam48@users.noreply.github.com>
2019-03-07 22:19:52 +0700

    Add files via upload

---
+++ MenampilkanBiodata.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menampilkan.biodata;
+
+/**
+ *
+ * @author Asus
+ */
+public class MenampilkanBiodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("nama : khaerul anam\n");
+        System.out.println("nim : 17090099\n");
+        System.out.println("kelas : 4A\n");
+        // TODO code application logic here
+    }
+    
+}
